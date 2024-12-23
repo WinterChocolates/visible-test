49f40ba462a90d8c154b21a311d7ca7cc49a1d85
柠檬冲水 <916415899@qq.com>
2024-12-23 11:02:21 +0800

    Update index.sh

--- centos/index.sh
+++ centos/index.sh
@@ -2,48 +2,46 @@
 
 source /visible/globals.sh
 
-wget --version 1>/dev/null
-if [ $? != 0 ]
-then yum  install wget -y
+# 检查 wget 是否安装，未安装则安装
+wget --version &>/dev/null
+if [ $? != 0 ]; then
+    yum install wget -y
 fi
 
-git version 1>/dev/null
-if [ $? != 0 ]
-then
-yum  install git -y
+# 检查 git 是否安装，未安装则安装
+git --version &>/dev/null
+if [ $? != 0 ]; then
+    yum install git -y
 fi
 
-while true
-do
+while true; do
     OPTION=$(whiptail \
-    --title "《visible》" \
-    --menu "$version" \
-    15 50 6 \
-    "1" "环境部署Environment" \
-    "2" "工具管理ToolManagement" \
-    "3" "系统重启Restart" \
-    3>&1 1>&2 2>&3)
+        --title "《Visible》" \
+        --menu "$version" \
+        15 50 3 \
+        "1" "环境部署 Environment" \
+        "2" "工具管理 Tool Management" \
+        "3" "系统重启 Restart" \
+        3>&1 1>&2 2>&3)
+
     feedback=$?
     if [ $feedback = 0 ]; then
-        # 环境部署administrat
+        # 环境部署
         if [ $OPTION = 1 ]; then
             bash "$centos/environment.sh"
         fi
 
-        # shellupdata
-        if [ $OPTION = 4 ]; then
+        # 工具管理 (示例功能，具体根据需求修改)
+        if [ $OPTION = 2 ]; then
             bash "$centos/shellupdata.sh"
-            child_exit_code=$?
-            if [ $child_exit_code -eq 0 ]; then
-                exit  # 结束整个脚本的执行
-            fi
         fi
 
-        # 系统重启restart
-        if [ $OPTION = 7 ]; then
+        # 系统重启
+        if [ $OPTION = 3 ]; then
             shutdown -r now
+            break # 系统重启后退出脚本
         fi
     else
-        exit
+        exit # 退出脚本
     fi
 done
